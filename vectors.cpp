58da681763c7905892243e41d0a0c6c886c7759a
TheSoyBean <91149324+TheSoyBean@users.noreply.github.com>
2024-09-09 21:24:37 +0000

    sync

---
+++ vectors.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+    // initializing vectors
+    
+    // adding item with push_back()
+    
+    // printing first and last item using at and size
+    
+    // remove last item with pop_back
+    
+    // print the last item using at
+    
+    // remove second item using erase
+    
+    // print the 2nd item using at
+    
+    // change the second item using at
+    
+    // print the second item using at
+    
+    // insert an item at index 1 using insert
+    
+    // print index 1 using at
+    
+    // for loop to print each item on a new line
+    
+    return 0;
+}
