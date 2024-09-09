376bad8b1990f01e87d7efd21b05b3d1b194192e
TheSoyBean <91149324+TheSoyBean@users.noreply.github.com>
2024-09-09 21:27:11 +0000

    sync

--- vectors.cpp
+++ vectors.cpp
@@ -4,14 +4,29 @@
 
 int main() {
     // initializing vectors
+    std::vector<std::string> cheeses({"cheddar", "bellavitano", "brie", "feta", "gouda"});
     
     // adding item with push_back()
+    cheeses.push_back("swiss");
     
     // printing first and last item using at and size
+    std::cout << "First and last item in list:\n";
+    
+    int end = cheeses.size() - 1;
+    
+    std::cout << cheeses.at(0) << "\n";
+    
+    std::cout << cheeses.at(end) << "\n\n";
     
     // remove last item with pop_back
+    cheeses.pop_back();
     
     // print the last item using at
+    std::cout << "Last item after pop_back()\n";
+    
+    int newEnd = cheeses.size() - 1;
+    
+    std::cout << cheeses.at(newEnd) << "\n\n";
     
     // remove second item using erase
     
