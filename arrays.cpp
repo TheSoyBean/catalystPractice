ed53f1dbf6f128865a998c35c2c8a0ade26bf520
Edward Sawyer Heaton <91149324+TheSoyBean@users.noreply.github.com>
2024-09-30 23:23:43 +0000

    print the matrix

--- arrays.cpp
+++ arrays.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 
 int main(){
+    //Initialized Matrix
     int matrix[3][3] = {
         {1, 2, 3}, 
         {4, 5, 6}, 
         {7, 8, 9}
     };
+
+    // Print the matrix
+    std::cout << "Matrix:" << std::endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            std::cout << std:: endl;
+        }
+    }
+
     return 0;
 }
